8c0cb6ed4ee0c91fee4a243b9690db2aef245c6d
Akash91655 <102722392+Akash91655@users.noreply.github.com>
2023-08-13 03:12:10 +0530

    Integration with razorpay (purchase.js)

--- ETAbackend1/routes/purchase.js
+++ ETAbackend1/routes/purchase.js
@@ -2,7 +2,6 @@ const purchaseController=require('../controllers/purchase');
 const express=require('express');
 const router=express.Router();
 const authMiddleware=require('../middleware/auth');
-
 router.get('/membership',authMiddleware.authentication,purchaseController.purchasePremium);
 router.post('/updatetransactionstatus',authMiddleware.authentication,purchaseController.updateTransaction);
 
