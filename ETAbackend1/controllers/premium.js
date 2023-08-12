ecb3f49cfbf96572197408a08f5125b62b9c05f2
Akash91655 <102722392+Akash91655@users.noreply.github.com>
2023-08-13 04:03:53 +0530

    Optimisation of premium feature leaderboard (premium.js)

--- ETAbackend1/controllers/premium.js
+++ ETAbackend1/controllers/premium.js
@@ -1,13 +1,19 @@
 const user=require('../model/user');
 const expense=require('../model/expense');
-
 const Sequelize=require('sequelize');
 const sequelize = require('../util/database');
 
 exports.showleader= async (req,res,next)=>{
     try{
         const leaders=await user.findAll({
-            attributes:['name','totalamount'],
+            attributes:['id','name',[Sequelize.fn('sum',sequelize.col('expenses.amount')),'totalamount']],
+            include:[
+                {
+                    model:expense,
+                    attributes:[]
+                }
+            ],
+            group:['user.id'],
             order:[['totalamount','DESC']]
         })
         res.json(leaders);
